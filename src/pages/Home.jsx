6976c6523399fa5ab9cff7f5e7902a035d7e469f
Filippo Brancato <brnfpp@gmail.com>
2026-06-17 16:48:25 +0200

    aggiunta home page con foto famiglia2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,39 +4,31 @@ function Home() {
   const navigate = useNavigate()
 
   return (
-    <div onClick={() => navigate("/lista")} style={{
-      width: "100%",
-      height: "100vh",
-      cursor: "pointer",
-      position: "relative",
-      overflow: "hidden"
+  <div onClick={() => navigate("/lista")} style={{
+    width: "100vw",
+    height: "100vh",
+    cursor: "pointer",
+    backgroundImage: "url('/famiglia.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center top",
+    position: "relative"
+  }}>
+    <div style={{
+      position: "absolute",
+      bottom: 60,
+      left: 0,
+      right: 0,
+      textAlign: "center",
+      color: "white",
+      fontSize: 14,
+      letterSpacing: 2,
+      textTransform: "uppercase",
+      opacity: 0.8
     }}>
-      <img
-        src="/famiglia.jpg"
-        alt="famiglia"
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          objectPosition: "center top"
-        }}
-      />
-      <div style={{
-        position: "absolute",
-        bottom: 60,
-        left: 0,
-        right: 0,
-        textAlign: "center",
-        color: "white",
-        fontSize: 14,
-        letterSpacing: 2,
-        textTransform: "uppercase",
-        opacity: 0.8
-      }}>
-        tocca per entrare
-      </div>
+      tocca per entrare
     </div>
-  )
+  </div>
+)
 }
 
 export default Home
