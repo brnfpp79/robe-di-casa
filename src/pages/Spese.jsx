bb28bc5f890e32804ca238f4cb1851b952b61459
Filippo Brancato <brnfpp@gmail.com>
2026-06-18 16:56:32 +0200

    ottimizzazione layout nuova spesa5

--- src/pages/Spese.jsx
+++ src/pages/Spese.jsx
@@ -62,7 +62,8 @@ function Pannello({ titolo, spesa, onSalva, onChiudi }) {
       bottom: 0,              // attaccato al fondo dello schermo
       left: "50%",            // parte dal centro
       transform: "translateX(-50%)", // si sposta a sinistra di metà della sua larghezza → risultato: centrato
-      width: 390,             // larghezza fissa uguale all'app
+      width: "100vw",
+      maxWidth: 390,             // larghezza fissa uguale all'app
       boxSizing: "border-box",
       background: "white",
       borderTop: "1px solid #eee",
